dda051189c103cdd81362a616b6db6cd6150155f
edurnemarco <emarco35@uoc.edu>
2025-01-09 09:51:10 +0100

    arreglar cambios direccio de arte y script.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-const sharp = require('sharp');
-   
-// create thumbnails
-  (async function () {
-    try {
-        await sharp('img/tapia-cerca1.jpg')
-        .resize (128,64)
-        .toFile('thumbnails/newimage.jpg')
-    } catch (error) {
-        console.log(error)
-    }
-  })();
-
+import './script.js';
